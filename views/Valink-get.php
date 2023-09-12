b5b5aa1529c8c0f714cf9d27d0191c997dd9135f
石賀悠 <ishigayuu@ishigayuunoiMac-7.local>
2023-09-12 11:22:05 +0900

    修正

--- views/Valink-get.php
+++ views/Valink-get.php
@@ -1,11 +1,12 @@
 <div class="wrap">
     <h1>登録</h1>
-    <?php if (get_transient('Valink')) : ?>
-        <p><?php echo get_transient('Valink'); ?></p>
-    <?php endif; ?>
+
     <form method="post" action="<?php echo menu_page_url('Valink', false) . '&action=save'; ?>">
         <?php wp_nonce_field('Valink-save', 'name_of_nonce_field'); ?>
         <input type="text" name="hoge" value="">
-        <button class="button button-primary">登録</button>
+        <button class="button button-primary">取得</button>
     </form>
+    <?php if (get_transient('Valink')) : ?>
+        <p><?php echo get_transient('Valink'); ?></p>
+    <?php endif; ?>
 </div>
